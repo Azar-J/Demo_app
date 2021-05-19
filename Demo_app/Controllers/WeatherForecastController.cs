1fa4529b800488482e6f3e22ba8a4933807e95c3
Azar J <azaarjam07@outlook.com>
2021-05-19 11:52:48 +0530

    commit change from freezing to freeze

--- Demo_app/Controllers/WeatherForecastController.cs
+++ Demo_app/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Demo_app.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freeze", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
